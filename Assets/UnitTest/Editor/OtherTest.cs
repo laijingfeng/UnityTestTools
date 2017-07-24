0a510d53dcf899bdc10c14aeb516480fca10c933
laijingfeng <337292616@qq.com>
2017-07-24 12:31:08 +0800

    update

---
+++ Assets/UnitTest/Editor/OtherTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+public class OtherTest
+{
+    [Test]
+    [MaxTime(100)]
+    public void SlowTest()
+    {
+        Thread.Sleep(200);
+    }
+}
